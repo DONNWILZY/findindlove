82c8896737a7dd384578cf693142681763b310a1
donnwilzy <ask4godswill@gmail.com>
2024-02-12 22:32:36 +0100

    reaction schema added

---
+++ models/Reactions.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const ReactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    reactionType: {
+        type: String,
+        enum: ['like', 'love', 'clap', 'dislike']
+    },
+    activityType: {
+        type: String,
+        required: true,
+        enum: ['post', 'news', 'videoContent', 'postComment', 'postReply', 'newsComment', 'newsReply', 'videoContentComment', 'videoContentReply']
+    },
+    activityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+}, { timestamps: true });
+
+const Reaction = mongoose.model('Reaction', ReactionSchema);
+
+module.exports = Reaction;
